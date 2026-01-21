b6949a5677a7b4ccf46b936ca05691b2a9f697d1
merserch <merserch@OA-S141-07>
2026-01-21 10:29:23 -0500

    Made an array and a random number picker for that array, changing the sprite on any key press.

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -5,30 +5,35 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public Sprite[] barrels;
+    public int randomSpriteIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PickARandomColour();
-        
+        PickARandomSprite();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(Keyboard.current.anyKey.wasPressedThisFrame)
-        //{
-        //    //PickARandomColour();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            //PickARandomColour();
+            PickARandomSprite();
+        }
 
         //get the mouse position
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         //is the mouse over the sprite?
         if(spriteRenderer.bounds.Contains(mousePos))
         {
+            //set the colour while hovering
             spriteRenderer.color = col;
         }
         else
         {
+            //otherwise set it back to white
             spriteRenderer.color = Color.white;
         }
     }
@@ -36,5 +41,14 @@ public class SpriteChanger : MonoBehaviour
     void PickARandomColour()
     {
         spriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandomSprite()
+    {
+        //get a random number that is the size of the barrels array
+        randomSpriteIndex = Random.Range(0, barrels.Length);
+        //use that to set the sprite
+        spriteRenderer.sprite = barrels[randomSpriteIndex];
+
     }
 }
